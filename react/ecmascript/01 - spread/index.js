b4bbf4e3c340c2788a7b4c2d3e58bb187f1ac97a
Brian Gerez <briangerez93@gmail.com>
2018-05-11 16:32:24 -0300

    Test 1

--- react/ecmascript/01 - spread/index.js
+++ react/ecmascript/01 - spread/index.js
@@ -1,8 +1,12 @@
 import { isArray } from './utils';
 
 export function min() {
-  isArray();
-  return Math.min();
+  if (arguments.length === 1) {
+    return arguments[0];
+  } else {
+    isArray();
+    return Math.min();
+  }
 }
 
 export function copy() {
